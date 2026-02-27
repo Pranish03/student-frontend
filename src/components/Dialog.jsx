66fb87ac6e94f85b602da6cbac37fa7163f6c64a
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-02-27 12:22:18 +0545

    Animate dialog component

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,9 +1,21 @@
+import { motion } from "framer-motion";
 import { LuX } from "react-icons/lu";
 
 export const Dialog = ({ heading, desc, close, children }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/10 backdrop-blur-xs">
-      <div className="bg-white border border-black/30 rounded-2xl w-110 p-4">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/10 backdrop-blur-xs"
+    >
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95, y: 20 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        exit={{ opacity: 0, scale: 0.95, y: 20 }}
+        transition={{ duration: 0.2, ease: "easeInOut" }}
+        className="bg-white border border-black/30 rounded-2xl w-110 p-4"
+      >
         <div className="text-gray-900 flex items-center justify-between mb-2">
           <h3 className="font-semibold text-xl">{heading}</h3>
           <button
@@ -16,7 +28,7 @@ export const Dialog = ({ heading, desc, close, children }) => {
         <p className="text-base text-gray-800 mb-5">{desc}</p>
 
         {children}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
