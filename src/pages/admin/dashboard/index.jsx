908e65830ef27dd3c8ff177142b7165511ed0c9a
Ankit.3k <ankitsangroula2006@gmail.com>
2026-03-04 20:19:27 +0545

    Merge test 2

--- src/pages/admin/dashboard/index.jsx
+++ src/pages/admin/dashboard/index.jsx
@@ -1,7 +1,13 @@
+import Chart from "../../admin/dashboard/chart";  
+
 export const AdminDashboard = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-zinc-900">Admin Dashboard</h1>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-gray-900">
+        Admin Dashboard
+      </h1>
+
+      <Chart />
     </div>
   );
 };
