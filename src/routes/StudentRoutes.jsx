4fdf4a2b0b15d103383ee88a8ebbb70d9cf8fc0a
Ankit.3k <ankitsangroula2006@gmail.com>
2026-03-08 15:57:27 +0545

    made manage notes page

--- src/routes/StudentRoutes.jsx
+++ src/routes/StudentRoutes.jsx
@@ -2,6 +2,7 @@ import { RequireAuth } from "../components/auth/RequireAuth";
 import { StudentLayout } from "../layouts/StudentLayout.jsx";
 import { ManageCourses } from "../pages/student/manage-courses";
 import { ManageAssignments } from "../pages/student/manage-assigments";
+import { ManageNotes } from "../pages/student/manage-notes";
 
 import { Account } from "../pages/shared/account";
 import { StudentDashboard } from "../pages/student/dashboard";
@@ -16,6 +17,7 @@ export const StudentRoutes = {
         { element: <StudentDashboard />, index: true },
         { element: <ManageCourses />, path: "manage-courses" },
         { element: <ManageAssignments />, path: "manage-assignments" },
+        { element: <ManageNotes />, path: "manage-notes" },
 
         { element: <Account />, path: "account" },
       ],
