1d62b3bdba683ff82fee19bd544f20b39a715332
Ankit.3k <ankitsangroula2006@gmail.com>
2026-03-08 15:54:59 +0545

    made manage assignmnets page

--- src/routes/StudentRoutes.jsx
+++ src/routes/StudentRoutes.jsx
@@ -1,6 +1,7 @@
 import { RequireAuth } from "../components/auth/RequireAuth";
 import { StudentLayout } from "../layouts/StudentLayout.jsx";
 import { ManageCourses } from "../pages/student/manage-courses";
+import { ManageAssignments } from "../pages/student/manage-assigments";
 
 import { Account } from "../pages/shared/account";
 import { StudentDashboard } from "../pages/student/dashboard";
@@ -14,6 +15,7 @@ export const StudentRoutes = {
       children: [
         { element: <StudentDashboard />, index: true },
         { element: <ManageCourses />, path: "manage-courses" },
+        { element: <ManageAssignments />, path: "manage-assignments" },
 
         { element: <Account />, path: "account" },
       ],
